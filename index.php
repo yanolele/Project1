49b2eb3fd75b66543e469e1f3e56992732b3b081
zonaylc <33519861+zonaylc@users.noreply.github.com>
2017-11-13 02:37:54 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -8,6 +8,58 @@
   <link href="index_style.css" rel="stylesheet" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	    <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react.min.js"></script>
+    <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react-dom.min.js"></script>
+    <script src="/bundles/jquery?v=JzhfglzUfmVF2qo-weTo-kvXJ9AJvIRBLmu11PgpbVY1"></script>
+
+<style>
+div.gallery { /*圖片的外框大小*/
+    border: 1px solid #ccc;
+}
+
+div.gallery:hover {
+    border: 1px solid #777;
+}
+
+div.gallery img {
+    width: 100%;
+    height: auto;
+}
+
+div.desc { /* Add desc...區塊的大小*/
+    padding: 15px;
+    text-align: center;
+}
+
+* {
+    box-sizing: border-box;
+}
+
+.responsive {
+    padding: 0 6px;
+    float: left;
+    width: 30%;
+}
+
+@media only screen and (max-width: 700px){
+    .responsive {
+        width: 49.99999%;
+        margin: 6px 0;
+    }
+}
+
+@media only screen and (max-width: 500px){
+    .responsive {
+        width: 100%;
+    }
+}
+
+.clearfix:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+</style>
 </head>
 <body class="body">
 <?php
@@ -18,12 +70,33 @@
 	  header('Location:login.php');
   }
   else{
-	   echo $_SESSION['uname'].'您好!'.'<a href="logout.php">登出</a>' ;
+	   echo '<td align="center">'.$_SESSION['uname'].'您好!'.'<a href="logout.php" >登出</a>' ;
   }
 ?>
-<div class="container">
+<div class="container" align="right">
+  <aside class="side1" >
+  	<h1 align="center">Let's Swap!</h1>
+    
+                    <li class="secnav-right__profile file" >
+    <a href=""><span><img src="head.png" width="64" height="64" alt=""/></span></a>
+    <div class="subnav subnav--profile subnav--cart-file">
+        <div class="subnav--profile__content">
+            <div class="profile">
+                <div class="nanoscroll__content">
+				<a class="subnav--cart__btn" href="">個人檔案</a>
+                </div>
+            </div>
+        </div>
+        <div class="subnav--cart__ft">
+            <a class="subnav--cart__btn" href="">我的願望清單</a>
+        </div>
+    </div>
+    <!-- /subnav--cart -->
+</li>
+
+  </aside>
   <div class="jumbotron type">
-  <h1 align="center">Let's Swap!</h1>
+  
     <div class="btn-group btype" role="group">
       
       <div class="btn-group" role="group">
@@ -46,23 +119,49 @@
       <button type="button" class="btn btn-default B6">文具</button>
     </div>
   </div>
-  <div class="row rs">
-    <div class="col-sm-4 cs">
-      <h3>Item</h3>
-      <img src=".jpg" alt=""/>
-      <p>Description...</p>
-      <button type="button" class="btn btn-sm btn-default like"></button>
+  
+  
+  <div id="wall">
+  
+  
+  <div class="responsive">
+  	  <div class="gallery">
+    <a target="_blank" href="img_fjords.jpg">
+      <img src="img_fjords.jpg" alt="Trolltunga Norway" width="256" height="256">
+    </a>
+    <div class="desc">Add a description of the image here <br>
+    <button type="button" class="btn btn-sm btn-default like"></button>
     </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+ 		</div>
+</div>
+
+<div class="responsive">
+		<div class="gallery"> 
+    <a target="_blank" href="img_fjords.jpg">
+      <img src="img_fjords.jpg" alt="Trolltunga Norway" width="256" height="256">
+    </a>
+    <div class="desc">Add a description of the image here <br>
+    <button type="button" class="btn btn-sm btn-default like"></button>
     </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+		</div> 
+</div>
+
+<div class="responsive">
+		  <div class="gallery">
+    <a target="_blank" href="img_fjords.jpg">
+      <img src="img_fjords.jpg" alt="Trolltunga Norway" width="256" height="256">
+    </a>
+    <div class="desc">Add a description of the image here <br>     
+    <button type="button" class="btn btn-sm btn-default like"></button>
     </div>
+ 		</div>
+</div>
+  
+      
+  </div>
+      
+    </div>
+  
   </div>
 </div>
 </body>
